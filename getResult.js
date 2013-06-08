039cc4047d1fb4a55668bde5cf4b2f9042d67931
web20opensource <web2.0opensource@gmail.com>
2013-06-08 13:42:58 -0500

    Update getResult.js
    Pool connection added

--- getResult.js
+++ getResult.js
@@ -8,14 +8,27 @@ var id;
 var body;
 
 //mysql + conf
+
 var _mysql = require('mysql');
-var HOST = 'localhost';
+/*var HOST = 'localhost';
 var PORT = 3306;
-var MYSQL_USER = 'nodejs';
-var MYSQL_PASS = '123456';
+var MYSQL_USER = 'root';
+var MYSQL_PASS = '';
+*/
 var DATABASE = 'resultados_2011';
 var TABLE = 'resultados_2011';
 
+
+var pool  = _mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : ''
+});
+
+
+
+
+/*
 //mysqlConnection
 var mysql = _mysql.createConnection({
     host: HOST,
@@ -27,7 +40,9 @@ var mysql = _mysql.createConnection({
 //console.log(mysql);
 //return;
 //use database
-mysql.query('use ' + DATABASE);
+*/
+
+
 
 
 // createServer + doQuery
@@ -37,19 +52,43 @@ http.createServer(function (req, res) {
     var url_parts = url.parse(req.url, true);
     var id = url_parts.query.id; 
 
-    // create&execute query
-    mysql.query('select * from ' + TABLE + ' where matricula ="'+id+'";',
-    function(err, result, fields) {
-        body = result[0]
-        if (err) throw err;
-        else {
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                if (typeof body === "undefined")
-                    response = '[{}]';
+    pool.getConnection(function(err, connection) {
+        
+        /*if (typeof err  !== 'undefined'){
+            console.log('ok');
+            console.dir(typeof err);
+        }
+        else{
+            console.log('shit!!!');
+            console.dir(typeof err);
+        }
+        return;*/
+
+        connection.query('use ' + DATABASE);
+        connection.query('select * from ' + TABLE + ' where matricula ="'+id+'";',
+            function(err, result, fields) {
+               /* if (typeof err === 'undefined')
+                    console.log(err.keys.length);
                 else
-                    response = '['+JSON.stringify(body)+']';
-                res.write(response);
-                res.end();
-            }
+                    console.log(err.keys.length);
+                return;*/
+
+                body = result
+                //console.log(result.length);
+                //return;
+                if (err) throw err;
+                else {
+                        //console.log('Row next to be json');
+                        //console.log(JSON.stringify(result[0]));
+                        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+                        if (typeof body === "undefined")
+                            response = '[{}]';
+                        else
+                            response = '['+JSON.stringify(body)+']';
+                        res.write(response);
+                        res.end();
+                    }
+            });
+        connection.end();        
     });
-}).listen(1337, 'localhost');
+}).listen(1337, '67.207.139.8');
